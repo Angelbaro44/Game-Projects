6b2741572a95b9b3d33058210846644f0b1d1f0a
Angel Baro <angelbaro44@gmail.com>
2020-07-17 23:37:19 -0400

    test

--- Pokemon-Championship/scr/index.js
+++ Pokemon-Championship/scr/index.js
@@ -1,20 +1,21 @@
 
-let userScore = 0;
-let compScore = 0;
+let userScore        = 0;
+let compScore        = 0;
 
-let userScore_span = document.getElementById("user-score");
-let compScore_span = document.getElementById("comp-score");
+let userScore_span   = document.getElementById("user-score");
+let compScore_span   = document.getElementById("comp-score");
 const scoreBoard_div = document.querySelector(".nametag");
-const result_p = document.querySelector(".result > p");
-const rock_div = document.getElementById("rock");
-const paper_div = document.getElementById("paper");
-const scissors_div = document.getElementById("scissors");
+const result_p       = document.querySelector(".result > p");
+const rock_div       = document.getElementById("rock");
+const paper_div      = document.getElementById("paper");
+const scissors_div   = document.getElementById("scissors");
+
+var gameTrack        = createAudioFromFile("image/cynthia-theme.mp3");
+var win              = createAudioFromFile("image/win1.mp3");
+var lose             = createAudioFromFile("image/lose1.wav");
+var draw             = createAudioFromFile("image/tie1.wav");
 
 
-var gameTrack = createAudioFromFile("image/cynthia-theme.mp3");
-var win = createAudioFromFile("image/win1.mp3");
-var lose = createAudioFromFile("image/lose1.wav");
-var draw = createAudioFromFile("image/tie1.wav");
 
 function createAudioFromFile(filename) {
 	var track = document.createElement("audio");
@@ -22,13 +23,12 @@ function createAudioFromFile(filename) {
 	return track;
 }
 
-
 function getCompChoice() {
 	return ["rock", "paper", "scissors"][Math.round(Math.random() * 3)];
 }
 
 function game(userChoice) {
-	window.onclick = hideChoices();
+	window.onclick   = hideChoices();
 	const compChoice = getCompChoice();
 	switch (userChoice + compChoice) {
 		case "rockscissors":
@@ -51,7 +51,7 @@ function game(userChoice) {
 
 function battle(gameCondition,userChoice,compChoice) {
 	if (gameCondition !== draw) {
-		gameCondition === win ? userScore += 100 : compScore += 100;
+	    gameCondition === win ? userScore += 100 : compScore += 100;
 	}
 	gameCondition.pause();
 	gameCondition.load();
@@ -59,12 +59,10 @@ function battle(gameCondition,userChoice,compChoice) {
 	userScore_span.innerHTML = userScore;
 	compScore_span.innerHTML = compScore;
 	showCompChoice(compChoice);
-	displayMessage(gameCondition,userChoice,compChoice)
+	displayMessage(gameCondition,userChoice,compChoice);
 }
 
-
 function displayMessage(gameCondition,userChoice,compChoice) {
-	console.log(gameCondition,userChoice,compChoice)
 	switch (gameCondition) {
 		case win:
 			result_p.innerHTML = `Your <span style="color:#00E4F2;">${makeWord(userChoice)}</span>-type Pokemon beat Cynthia's <span style="color:#F9F345;">${makeWord(compChoice)}</span>-type Pokemon!<br> You won this match - Ready your next Pokemon.`;
@@ -76,8 +74,7 @@ function displayMessage(gameCondition,userChoice,compChoice) {
 			result_p.innerHTML = `Your <span style="color:#00E4F2;">${makeWord(userChoice)}</span>-type Pokemon is the same type as Cynthia's <span style="color:#F9F345;">${makeWord(compChoice)}</span> type Pokemon.<br> It's a draw! Both Pokemon fainted.`;
 			break;
 	}
-
-	}
+}
 
 	function makeWord(letter) {
 		if (letter === "rock") return "Grass";
@@ -86,8 +83,6 @@ function displayMessage(gameCondition,userChoice,compChoice) {
 	}
 
 	function showCompChoice(compChoice) {
-
-
 		if (compChoice === "rock") {
 			rock_div.classList.add("computorChoice");
 		}
@@ -99,10 +94,9 @@ function displayMessage(gameCondition,userChoice,compChoice) {
 		}
 	}
 
-
 	function hideChoices() {
-		rock_div.classList.remove("computorChoice", "playerChoice");
-		paper_div.classList.remove("computorChoice", "playerChoice");
+		rock_div.classList.remove    ("computorChoice", "playerChoice");
+		paper_div.classList.remove   ("computorChoice", "playerChoice");
 		scissors_div.classList.remove("computorChoice", "playerChoice");
 	}
 
@@ -111,12 +105,10 @@ function displayMessage(gameCondition,userChoice,compChoice) {
 			game("rock");
 			rock_div.classList.add("playerChoice");
 		});
-
 		paper_div.addEventListener('click', function () {
 			game("paper");
 			paper_div.classList.add("playerChoice");
 		});
-
 		scissors_div.addEventListener('click', function () {
 			game("scissors");
 			scissors_div.classList.add("playerChoice");
@@ -124,5 +116,7 @@ function displayMessage(gameCondition,userChoice,compChoice) {
 	};
 
 	main();
+
+
 	var vid = document.getElementById("myVideo");
 	vid.volume = 0.1;
